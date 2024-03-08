2ef0f3ba14b12566c792cf861b23b82e235685cd
jira-edu <jiapong.edu@gmail.com>
2024-03-08 15:39:42 +0700

    Add implement function for period board

--- include/score_lib.h
+++ include/score_lib.h
@@ -99,6 +99,10 @@ class Scorelib {
         }
 
         void display_period(bool n[34]) {
+
+            for (int i=0; i<35; i++) {
+                n[i] = !n[i];
+            }
             
             // Column 1
             u1[7] = n[0];
